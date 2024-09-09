5922d4096529b48c274bd838d1cd4de3de8a279c
habibur62 <habibur.vendabari@gmail.com>
2024-09-09 21:12:06 +0600

    page create

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2>Home Page</h2>
+        <p>Lorem ipsum dolor sit 
+            amet consectetur adipisicing elit. Alias explicabo porro iure cumque. Praesentium, quasi consequuntur. Fuga assumenda atque commodi eaque nobis, natus iure adipisci autem, saepe alias veritatis impedit eos consequuntur aperiam beatae hic similique ad laboriosam! Deserunt non fuga nisi similique in soluta placeat amet sed? Eligendi ex assumenda non similique accusantium quaerat tempora repellat. Aliquam obcaecati magnam eligendi quaerat veritatis, nam magni vel repellendus eius, maiores doloribus ipsa. Ducimus reiciendis soluta laborum libero, dicta eos doloremque minima ex laudantium veritatis, eligendi quo perspiciatis, porro neque eveniet rerum facere amet placeat vitae recusandae magni voluptate vero? Quos, ad.</p>
+    </div>
+  )
+}
