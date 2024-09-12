933b4a6919dc8c88ce7029efaaef50096bc21d9d
habibur62 <habibur.vendabari@gmail.com>
2024-09-12 12:07:58 +0600

    place order page create

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import '/PlaceOrder.css'
+import './PlaceOrder.css'
 
 export default function PlaceOrder() {
   return (
-    <div>
-        
-    </div>
+    <form className='place-order'>
+        <div className="place-order-left">
+            <h2>Delivery Information</h2>
+            <div className="multi-fields">
+                <input type="text" placeholder='First name' />
+                <input type="text" placeholder='Last name' />
+            </div>
+            <input type="email" placeholder='Email address'  />
+            <input type="text" placeholder='Street'  />
+            <div className="multi-fields">
+                <input type="text" placeholder='City' />
+                <input type="text" placeholder='State' />
+            </div>
+            <input type="number" placeholder='Phone' />
+
+
+        </div>
+        <div className="place-order-right">
+            <h3>Cart Totals</h3>
+        </div>
+    </form>
   )
 }
